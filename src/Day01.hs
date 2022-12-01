2ff56cfd6034313e0b547e08a315dc1fedac0254
Scott Sedgwick <ssedgwick@cochlear.com>
2022-12-01 17:08:29 +1100

    Moved common code from solver functions to parser function

--- src/Day01.hs
+++ src/Day01.hs
@@ -4,16 +4,16 @@ import qualified Data.Attoparsec.Text as A
 import Data.List (sort)
 import ParserUtils
 
-type Input = [[Int]]
+type Input = [Int]
 
 datafile :: FilePath
 datafile = "data/Day01.txt"
 
 parser :: A.Parser Input
-parser = A.many1 intGroup
+parser = (reverse . sort . map sum) <$> A.many1 intGroup
 
 part1 :: Input -> Int
-part1 = maximum . map sum
+part1 = head 
 
 part2 :: Input -> Int
-part2 = sum . take 3 . reverse . sort . map sum
+part2 = sum . take 3
