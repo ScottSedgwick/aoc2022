75e50e6902a541f4a8f06d7d3b62ec631b4ca1f1
Scott Sedgwick <ssedgwick@cochlear.com>
2022-12-06 17:47:45 +1100

    Completed Day 06.

--- src/Day06.hs
+++ src/Day06.hs
@@ -1,18 +1,27 @@
 module Day06 (Input, datafile, parser, part1, part2) where
 
--- import Control.Applicative ((<|>))
 import qualified Data.Attoparsec.Text as A
+import Data.List (nub)
+import ParserUtils ( strLine )
 
-type Input = Int
+type Input = String
 
 datafile :: FilePath
 datafile = "data/Day06.txt"
 
 parser :: A.Parser Input
-parser = undefined
+parser = strLine
 
-part1 :: Input -> String
-part1 = undefined
+part1 :: Input -> Maybe Int
+part1 = p 4 0 
 
-part2 :: Input -> String
-part2 = undefined
+p :: Int -> Int -> String -> Maybe Int
+p _ _ [] = Nothing
+p l n xs = if length xs < l
+           then Nothing
+           else if (length (nub (take l xs)) == l)
+                then Just (n + l)
+                else p l (n + 1) (tail xs)
+
+part2 :: Input -> Maybe Int
+part2 = p 14 0 
--- test/Tests/Test06.hs
+++ test/Tests/Test06.hs
@@ -8,9 +8,7 @@ import Day06 (Input, datafile, parser, part1, part2)
 
 tests :: Input -> Spec
 tests ys = describe "Day 06 tests" $ do
-    undefined
-    -- it "parser" $ stacks ys `shouldBe` M.fromList [(1,"NHSJFWTD"),(2,"GBNTQPRH"),(3,"VQL"),(4,"QRWSBN"),(5,"BMVTFDN"),(6,"RTHVBDM"),(7,"JQBD"),(8,"QHZRVJND"),(9,"SMHNB")]
-    -- it "part1" $ do
-    --   part1 ys `shouldBe` "GRTSWNJHH"
-    -- it "part2" $ do
-    --   part2 ys `shouldBe` "QLFQDBBHM"
+    it "part1" $ do
+      part1 ys `shouldBe` (Just 1892)
+    it "part2" $ do
+      part2 ys `shouldBe` (Just 2313)
